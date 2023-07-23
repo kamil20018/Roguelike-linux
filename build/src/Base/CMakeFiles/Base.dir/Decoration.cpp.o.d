9977091c438910d1a864b9ddd7a4367105303621
kamil20018 <matbiz01@gmail.com>
2023-07-24 01:35:03 +0200

    removed old build files and added git ignore

--- build/src/Base/CMakeFiles/Base.dir/Decoration.cpp.o.d
+++
@@ -1,4 +0,0 @@
-src/Base/CMakeFiles/Base.dir/Decoration.cpp.o: \
- /home/kamil/code/Roguelike/src/Base/Decoration.cpp \
- /usr/include/stdc-predef.h \
- /home/kamil/code/Roguelike/src/Base/Decoration.h
--- build/src/Settings/CMakeFiles/Settings.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Settings.
-# This may be replaced when dependencies are built.
